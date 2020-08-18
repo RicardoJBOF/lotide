430a31a05ac2b33fd2bc4e3d221fb4cd79cb6fa8
RicardoJBOF <ricardo.jbof@hotmail.com>
2020-08-18 00:53:53 +0000

    Send correct file - head.js

--- head.js
+++ head.js
@@ -14,7 +14,16 @@ const assertEqual = function(actual, expected) {
 
 };
 
-const head = function(actual, expected) {
+// TEST CODE
 
+
+const head = function(arrayTest){
+
+  let answer =  arrayTest.shift()
   
-}
+  return answer;
+
+};
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
