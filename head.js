887c6196f91fca236c83bc5555bc0c3274de8d3d
RicardoJBOF <ricardo.jbof@hotmail.com>
2020-08-18 00:50:20 +0000

    Create head.js

---
+++ head.js
@@ -0,0 +1,20 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    
+    console.log(`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
+  
+  } else {
+
+    console.log(`🔴🔴🔴 Assertion Failed ${actual} !== ${expected}`);
+
+  }
+
+
+};
+
+const head = function(actual, expected) {
+
+  
+}
