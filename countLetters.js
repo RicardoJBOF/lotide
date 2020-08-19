463e74bc17ebdb4e6f04dfdeb7b86e112c8a9f36
RicardoJBOF <ricardo.jbof@hotmail.com>
2020-08-19 20:19:24 +0000

    Add conutLetters

---
+++ countLetters.js
@@ -0,0 +1,42 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    
+    console.log(`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
+  
+  } else {
+
+    console.log(`🔴🔴🔴 Assertion Failed ${actual} !== ${expected}`);
+
+  }
+
+
+};
+
+const countLetters = function(arrayTest) {
+
+  const result = {};
+
+  for (let letters of arrayTest){
+
+      if (result[letters]){
+
+        result[letters]++
+        
+      } else {
+
+        result[letters] = 1;
+
+      }
+      
+
+  }
+
+  return result;
+
+}
+
+console.log(countLetters("hello"));
+
+// TEST CODE
