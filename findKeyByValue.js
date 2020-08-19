11e57f057c35f265118e338bf15ca2ba1f82b638
RicardoJBOF <ricardo.jbof@hotmail.com>
2020-08-19 23:34:20 +0000

    Add Find Key

---
+++ findKeyByValue.js
@@ -0,0 +1,43 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+      
+  if (actual === expected) {
+    
+    console.log(`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
+  
+  } else {
+
+    console.log(`🔴🔴🔴 Assertion Failed ${actual} !== ${expected}`);
+
+  }
+
+
+};
+
+// TEST CODE
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+const findKeyByValue = function(objectMovies, title) {
+
+  for (let movies of Object.keys(objectMovies)) {
+
+     if (objectMovies[movies] === title) {
+
+        return movies;
+
+     }
+
+  }
+
+};
+
+
+
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
