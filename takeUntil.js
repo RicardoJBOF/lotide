f68199ab5b6e645079649c4a30724cfc7c480d6c
RicardoJBOF <ricardo.jbof@hotmail.com>
2020-08-20 23:35:01 +0000

    Do takeUntil exercise

---
+++ takeUntil.js
@@ -0,0 +1,30 @@
+const takeUntil = function(array, callback) {
+  let answer = [];
+  for(const element of array) {
+    if(callback(element)) {
+    return answer;
+    } else {
+      answer.push(element)
+    }
+  }
+  return answer;
+}
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0); 
+console.log(results1); //[ 1, 2, 5, 7, 2 ]
+
+/*
+const takeUntil = function(array, callback) {
+  let answer = [];
+  for(let element of array) {
+    if(!callback(element)) {
+      answer.push(element)
+    } else if (callback(element)) {
+      break;
+    }
+  }
+  return answer;
+}
+
+*/
