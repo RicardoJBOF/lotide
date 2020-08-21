6e691da44f64da756c2884aa77f208c6ac2d1984
RicardoJBOF <ricardo.jbof@hotmail.com>
2020-08-21 18:30:31 +0000

    Retify eqObjects

--- eqObjects.js
+++ eqObjects.js
@@ -1,10 +1,10 @@
 // FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
+const assertEqual = function (actual, expected) {
 
   if (actual === expected) {
-    
+
     console.log(`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
-  
+
   } else {
 
     console.log(`🔴🔴🔴 Assertion Failed ${actual} !== ${expected}`);
@@ -14,7 +14,7 @@ const assertEqual = function(actual, expected) {
 
 };
 
-const eqArrays = function(actual, expected) {
+const eqArrays = function (actual, expected) {
 
   if (actual.toString() === expected.toString()) {
 
@@ -29,55 +29,53 @@ const eqArrays = function(actual, expected) {
 
 // Returns true if both objects have identical keys with identical values.
 // Otherwise you get back a big fat false!
-const eqObjects = function(object1, object2) {
-
-  let answer = '';
-
-  if(Object.keys(object1).length !== Object.keys(object2).length) {
-
-  answer = false;
-
+const eqObjects = (object1, object2) => {
+  if (Object.keys(object1).length !== Object.keys(object2).length) {
+    return false;
   } else {
-
-
-
-    for(let element of Object.keys(object1)) {
-
-      if(object1[element] !== object2[element]){
-
-        answer = false;
-
-        if (Object.values(object1[element]).isArray && Object.values(object2[element]).isArray) {
-
-          answer = eqArray(Object.values(object1[element]), Object.values(object2[element]))
-
-        };
-
+    for (const key in object1) {
+      if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
+        if (!eqArrays(object1[key], object2[key])) {
+          return false;
+        }
       } else {
-
-        answer = true;
-
+        if (!object1[key] === object2[key]) {
+          return false;
+        }
       }
-
-    } 
+    }
   }
-
-return answer;
-
+  return true;
 };
 
 
 // TEST CODE
 
-const ab = { a: "1", b: "2" };
-const ba = { b: "2", a: "1" };
-const abc = { a: "1", b: "2", c: "3" };
-const cd = { c: "1", d: ["2", 3] };
-const dc = { d: ["2", 3], c: "1" };
-const cd2 = { c: "1", d: ["2", 3, 4] };
+const ab = {
+  a: "1",
+  b: "2"
+};
+const ba = {
+  b: "2",
+  a: "1"
+};
+const abc = {
+  a: "1",
+  b: "2",
+  c: "3"
+};
+const cd = {
+  c: "1",
+  d: ["2", 3]
+};
+const dc = {
+  d: ["2", 3],
+  c: "1"
+};
+const cd2 = {
+  c: "1",
+  d: ["2", 3, 4]
+};
 
 
-console.log(eqObjects(ab, ba)); //false
-
-
-
+console.log(eqObjects(cd, cd2));
