7f22c7b6343ee390194aed5bc863443fdddff551
Ricardo Barbosa <62962969+RicardoJBOF@users.noreply.github.com>
2020-08-24 13:58:18 -0400

    Update assertEqual and head

--- assertEqual.js
+++ assertEqual.js
@@ -1,20 +1,13 @@
 // FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-
+const assertEqual = (actual, expected) => {
   if (actual === expected) {
-    
     console.log(`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
-  
   } else {
-
     console.log(`🔴🔴🔴 Assertion Failed ${actual} !== ${expected}`);
-
   }
-
-
 };
 
 // TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
 
+
+module.exports = assertEqual;
--- head.js
+++ head.js
@@ -1,29 +1,11 @@
+const assertEqual = require('./assertEqual');
+
 // FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
 
-  if (actual === expected) {
-    
-    console.log(`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
-  
-  } else {
-
-    console.log(`🔴🔴🔴 Assertion Failed ${actual} !== ${expected}`);
-
-  }
-
-
-};
-
-// TEST CODE
-
-
-const head = function(arrayTest){
-
-  let answer =  arrayTest.shift()
-  
+const head = arrayTest => {
+  let answer = arrayTest.shift()
   return answer;
-
 };
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head;
+
