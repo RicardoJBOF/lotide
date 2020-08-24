3d8dea03c419814d526cef85787b5156075313de
RicardoJBOF <ricardo.jbof@hotmail.com>
2020-08-24 18:20:08 +0000

    Update eqArrays and head

--- eqArrays.js
+++ eqArrays.js
@@ -1,34 +1,14 @@
 // FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
 
-  if (actual === expected) {
-    
-    console.log(`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
-  
-  } else {
-
-    console.log(`🔴🔴🔴 Assertion Failed ${actual} !== ${expected}`);
-
-  }
-
-
-};
-
-
-const eqArrays = function(actual, expected) {
-
+const eqArrays = (actual, expected) => {
   if (actual.toString() === expected.toString()) {
-
     return true;
-
   } else {
-
     return false;
   }
-
 };
 
-// TEST CODE
+module.exports = eqArrays;
 
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+
