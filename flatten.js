89193dddded0393d0be9d7ad519fa9b4d95a9c86
RicardoJBOF <ricardo.jbof@hotmail.com>
2020-11-10 17:55:13 -0500

    flatten.js

---
+++ flatten.js
@@ -0,0 +1,23 @@
+//IMPORT FILE
+const eqArrays = require("./eqArrays");
+const assertArraysEqual = require("./assertArraysEqual");
+
+// FUNCTION IMPLEMENTATION
+const flatten = (array) => {
+  const answer = [];
+
+  for (let i of array) {
+    if (!Array.isArray(i)) {
+      answer.push(i);
+    } else {
+      for (let j of i) {
+        answer.push(j);
+      }
+    }
+  }
+
+  return answer;
+};
+
+//TEST CODE
+//console.log(flatten([1, 2, [3, 4], 5, [6]])); // => [1, 2, 3, 4, 5, 6]
