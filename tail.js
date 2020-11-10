62b6542c9985d214a53b97aaa99b362175662d52
RicardoJBOF <ricardo.jbof@hotmail.com>
2020-11-10 11:39:03 -0500

    Refactoring tail.js

--- tail.js
+++ tail.js
@@ -1,11 +1,15 @@
-const assertEqual = require('./assertEqual');
+//IMPORT FILE
+const assertEqual = require("./assertEqual");
 
 // FUNCTION IMPLEMENTATION
+const tail = (array) => array.slice(1);
 
-const tail = arrayTest => {
-  arrayTest.shift();
-  return arrayTest;
-};
+// TEST CODE
+// console.log(tail([1, 2, 3, 4, 5]));
+// const result = tail(["Yo Yo", "Lighthouse", "Labs"]);
+// assertEqual(result.length, 2);
+// assertEqual(result[0], "Lighthouse");
+// assertEqual(result[1], "Labs");
 
-
+//EXPORT FILE
 module.exports = tail;
