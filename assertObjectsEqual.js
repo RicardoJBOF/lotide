d038136d251349efbc27f54ff1bca77a176e0963
RicardoJBOF <ricardo.jbof@hotmail.com>
2020-11-11 18:20:57 -0500

    Refactoring assertObjectsEqual.js

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,70 +1,41 @@
+//IMPORT FILE
+const eqObjects = require("./eqObjects");
+
 // FUNCTION IMPLEMENTATION
-
-const eqArrays = function(actual, expected) {
-  if (actual.toString() === expected.toString()) {
-    return true;
-  } else {
-    return false;
-  }
+const assertObjectsEqual = function (actual, expected) {
+  eqObjects
+    ? console.log(`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`)
+    : console.log(`🔴🔴🔴 Assertion Failed ${actual} !== ${expected}`);
 };
-
-const eqObjects = (object1, object2) => {
-  if (Object.keys(object1).length !== Object.keys(object2).length) {
-    return false;
-  } else {
-    for (const key in object1) {
-      if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
-        if (!eqArrays(object1[key], object2[key])) {
-          return false;
-        }
-      } else {
-        if (!object1[key] === object2[key]) {
-          return false;
-        }
-      }
-    }
-  }
-  return true;
-};
-
-const assertObjectsEqual = function(actual, expected) {
-  if (eqObjects) {
-    console.log(`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🔴🔴🔴 Assertion Failed ${actual} !== ${expected}`);
-  }
-
-};
-
-module.exports = assertObjectsEqual;
 
 // TEST CODE
-
 const ab = {
   a: "1",
-  b: "2"
+  b: "2",
 };
 const ba = {
   b: "2",
-  a: "1"
+  a: "1",
 };
 const abc = {
   a: "1",
   b: "2",
-  c: "3"
+  c: "3",
 };
 const cd = {
   c: "1",
-  d: ["2", 3]
+  d: ["2", 3],
 };
 const dc = {
   d: ["2", 3],
-  c: "1"
+  c: "1",
 };
 const cd2 = {
   c: "1",
-  d: ["2", 3, 4]
+  d: ["2", 3, 4],
 };
 
+// assertObjectsEqual(cd, cd2);
 
-//assertObjectsEqual(cd, cd2);
+//EXPORT FILE
+module.exports = assertObjectsEqual;
