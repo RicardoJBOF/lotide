923f4fcd6fd06b2a888a27b815fdf1a880738316
RicardoJBOF <ricardo.jbof@hotmail.com>
2020-11-11 17:52:19 -0500

    Refactoring findKeyByValue.js

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,42 +1,24 @@
+//IMPORT FILE
+const assertEqual = require("./assertEqual");
+
 // FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-      
-  if (actual === expected) {
-    
-    console.log(`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
-  
-  } else {
-
-    console.log(`🔴🔴🔴 Assertion Failed ${actual} !== ${expected}`);
-
+const findKeyByValue = function (objectMovies, title) {
+  for (const genre of Object.keys(objectMovies)) {
+    if (objectMovies[genre] === title) {
+      return genre;
+    }
   }
-
-
 };
 
 // TEST CODE
-
-const bestTVShowsByGenre = { 
+const bestTVShowsByGenre = {
   sci_fi: "The Expanse",
   comedy: "Brooklyn Nine-Nine",
-  drama:  "The Wire"
+  drama: "The Wire",
 };
 
-const findKeyByValue = function(objectMovies, title) {
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
 
-  for (let movies of Object.keys(objectMovies)) {
-
-     if (objectMovies[movies] === title) {
-
-        return movies;
-
-     }
-
-  }
-
-};
-
+//EXPORT FILE
 module.exports = findKeyByValue;
-
-// assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
-// assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
