46f3c90efc70152086dfe1218d1a9fd355d23d7e
RicardoJBOF <ricardo.jbof@hotmail.com>
2020-11-11 18:17:25 -0500

    Refactoring eqObjects.js

--- eqObjects.js
+++ eqObjects.js
@@ -1,34 +1,9 @@
+//IMPORT FILE
+const assertEqual = require("./assertEqual");
+const eqArrays = require("./eqArrays");
+
 // FUNCTION IMPLEMENTATION
-const assertEqual = function (actual, expected) {
 
-  if (actual === expected) {
-
-    console.log(`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
-
-  } else {
-
-    console.log(`🔴🔴🔴 Assertion Failed ${actual} !== ${expected}`);
-
-  }
-
-
-};
-
-const eqArrays = function (actual, expected) {
-
-  if (actual.toString() === expected.toString()) {
-
-    return true;
-
-  } else {
-
-    return false;
-  }
-
-};
-
-// Returns true if both objects have identical keys with identical values.
-// Otherwise you get back a big fat false!
 const eqObjects = (object1, object2) => {
   if (Object.keys(object1).length !== Object.keys(object2).length) {
     return false;
@@ -48,35 +23,35 @@ const eqObjects = (object1, object2) => {
   return true;
 };
 
-
 // TEST CODE
-
 const ab = {
   a: "1",
-  b: "2"
+  b: "2",
 };
 const ba = {
   b: "2",
-  a: "1"
+  a: "1",
 };
 const abc = {
   a: "1",
   b: "2",
-  c: "3"
+  c: "3",
 };
 const cd = {
   c: "1",
-  d: ["2", 3]
+  d: ["2", 3],
 };
 const dc = {
   d: ["2", 3],
-  c: "1"
+  c: "1",
 };
 const cd2 = {
   c: "1",
-  d: ["2", 3, 4]
+  d: ["2", 3, 4],
 };
 
-module.exports = eqObjects;
+// console.log(eqObjects(cd, cd2));
+// console.log(eqObjects(ab, ba));
 
-//console.log(eqObjects(cd, cd2));
+//EXPORT FILE
+module.exports = eqObjects;
