462ac45fd024515223c84167d905e230d00509e5
RicardoJBOF <ricardo.jbof@hotmail.com>
2020-11-11 12:08:18 -0500

    Refactoring middle.js

--- middle.js
+++ middle.js
@@ -1,6 +1,8 @@
-// FUNCTION IMPLEMENTATION
+//IMPORT FILE
+const eqArrays = require("./eqArrays");
 const assertArraysEqual = require('./assertArraysEqual');
 
+// FUNCTION IMPLEMENTATION
 const middle = middleArray => {
   let middleArrayAnswer = [];
   if (middleArray.length > 2) {
@@ -13,5 +15,15 @@ const middle = middleArray => {
   return middleArrayAnswer;
 };
 
+//TEST CODE
+console.log(middle([1])) // => []
+console.log(middle([1, 2])) // => []
+console.log(middle([1, 2, 3])) // => [2]
+console.log(middle([1, 2, 3, 4, 5])) // => [3]
+console.log(middle([1, 2, 3, 4])) // => [2, 3]
+console.log(middle([1, 2, 3, 4, 5, 6])) // => [3, 4]
+
+
+//EXPORT FILE
 module.exports = middle;
 
