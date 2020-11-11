af9d06de49fbbd92b0e4a50f40c31ef86949b7c4
RicardoJBOF <ricardo.jbof@hotmail.com>
2020-11-11 12:09:36 -0500

    Refactoring middle.js

--- middle.js
+++ middle.js
@@ -16,12 +16,12 @@ const middle = middleArray => {
 };
 
 //TEST CODE
-console.log(middle([1])) // => []
-console.log(middle([1, 2])) // => []
-console.log(middle([1, 2, 3])) // => [2]
-console.log(middle([1, 2, 3, 4, 5])) // => [3]
-console.log(middle([1, 2, 3, 4])) // => [2, 3]
-console.log(middle([1, 2, 3, 4, 5, 6])) // => [3, 4]
+// console.log(middle([1])) // => []
+// console.log(middle([1, 2])) // => []
+// console.log(middle([1, 2, 3])) // => [2]
+// console.log(middle([1, 2, 3, 4, 5])) // => [3]
+// console.log(middle([1, 2, 3, 4])) // => [2, 3]
+// console.log(middle([1, 2, 3, 4, 5, 6])) // => [3, 4]
 
 
 //EXPORT FILE
