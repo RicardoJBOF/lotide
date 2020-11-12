c0347c63d09477636491e864530f3f2ab787c490
RicardoJBOF <ricardo.jbof@hotmail.com>
2020-11-12 16:12:11 -0500

    Refactoring findKey.js

--- findKey.js
+++ findKey.js
@@ -1,33 +1,29 @@
+//IMPORT FILE
+const assertEqual = require("./assertEqual");
+
 // FUNCTION IMPLEMENTATION
-
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🔴🔴🔴 Assertion Failed ${actual} !== ${expected}`);
-  }
-};
-
-
-const findKey = (object1, callback1) => { 
-  let answer = '';
-  for (const key in object1) {
-    if (callback1(object1[key])) {
+const findKey = (object, callback) => {
+  for (const key in object) {
+    if (callback(object[key])) {
       return key;
     }
   }
-  return answer;
 };
 
+// TEST CODE
+// console.log(
+//   findKey(
+//     {
+//       "Blue Hill": { stars: 1 },
+//       Akaleri: { stars: 3 },
+//       noma: { stars: 2 },
+//       elBulli: { stars: 3 },
+//       Ora: { stars: 2 },
+//       Akelarre: { stars: 3 },
+//     },
+//     (x) => x.stars === 2
+//   )
+// ); // => "noma"
 
-// console.log(findKey({
-//   "Blue Hill": { stars: 1 },
-//   "Akaleri":   { stars: 3 },
-//   "noma":      { stars: 2 },
-//   "elBulli":   { stars: 3 },
-//   "Ora":       { stars: 2 },
-//   "Akelarre":  { stars: 3 }
-// }, x => x.stars === 2)) // => "noma"
-
+//EXPORT FILE
 module.exports = findKey;
-
