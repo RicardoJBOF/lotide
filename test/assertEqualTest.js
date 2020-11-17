700b5e69306cedfc4736a8e82684dab5a30f92b8
RicardoJBOF <ricardo.jbof@hotmail.com>
2020-11-17 15:01:09 -0500

    Refactoring assertEqualTest.js headTest.js

--- test/assertEqualTest.js
+++ test/assertEqualTest.js
@@ -1,4 +1,17 @@
-const assertEqual = require('../assertEqual');
+//IMPORT FILE
+const assert = require("chai").assert;
+const assertEqual = require("../assertEqual");
 
-assertEqual(1,1);
-assertEqual(1,2);
+//TEST
+describe("#assertEqual", () => {
+  it('returns false for ("Lighthouse Labs", "Bootcamp")', () => {
+    assert.strictEqual(
+      assertEqual("Lighthouse Labs", "Bootcamp"),
+      `🔴🔴🔴 Assertion Failed Lighthouse Labs !== Bootcamp`
+    );
+  });
+
+  it("returns true for assertEqual(1,1)", () => {
+    assert.strictEqual(assertEqual(1, 1), `🟢🟢🟢 Assertion Passed: 1 === 1`);
+  });
+});
--- test/headTest.js
+++ test/headTest.js
@@ -1,19 +1,14 @@
-// const assertEqual = require('../assertEqual');
-// const head = require('../head');
-// assertEqual(head([5, 6, 7]), 5);
-// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+//IMPORT FILE
+const assert = require("chai").assert;
+const head = require("../head");
 
-const assert = require('chai').assert;
-const head   = require('../head');
-
+//TEST
 describe("#head", () => {
   it('returns "Hello" for ["Hello", "Lighthouse", "Labs"]', () => {
     assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
   });
 
   it("returns 5 for [5, 6, 7]", () => {
-    assert.strictEqual(head([5, 6, 7]), 5); 
+    assert.strictEqual(head([5, 6, 7]), 5);
   });
-
-
 });
