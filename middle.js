8ea531a9c7ea84fbf319435e21b0104c86b7c0b0
RicardoJBOF <ricardo.jbof@hotmail.com>
2020-11-18 17:52:25 -0500

    Refactoring middleTest.js

--- middle.js
+++ middle.js
@@ -15,15 +15,6 @@ const middle = middleArray => {
   return middleArrayAnswer;
 };
 
-//TEST CODE
-// console.log(middle([1])) // => []
-// console.log(middle([1, 2])) // => []
-// console.log(middle([1, 2, 3])) // => [2]
-// console.log(middle([1, 2, 3, 4, 5])) // => [3]
-// console.log(middle([1, 2, 3, 4])) // => [2, 3]
-// console.log(middle([1, 2, 3, 4, 5, 6])) // => [3, 4]
-
-
 //EXPORT FILE
 module.exports = middle;
 
--- test/middleTest.js
+++ test/middleTest.js
@@ -1,4 +1,26 @@
-const assertArraysEqual = require('../assertArraysEqual');
-const middle = require('../middle');
+//IMPORT FILE
+const assert = require("chai").assert;
+const middle = require("../middle");
 
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
+//TEST
+describe("#middle", () => {
+  it("returns [] for [1]", () => {
+    assert.equal(middle([1]).toString(), "");
+  });
+
+  it("returns [] for [1, 2]", () => {
+    assert.equal(middle([1, 2]).toString(), "");
+  });
+
+  it("returns [2] for [1, 2, 3]", () => {
+    assert.equal(middle([1, 2, 3]).toString(), "2");
+  });
+
+  it("returns [3] for [1, 2, 3, 4, 5]", () => {
+    assert.equal(middle([1, 2, 3, 4, 5]).toString(), "3");
+  });
+
+  it("returns [3, 4] for [1, 2, 3, 4, 5, 6]", () => {
+    assert.equal(middle([1, 2, 3, 4, 5, 6]).toString(), "3,4");
+  });
+});
